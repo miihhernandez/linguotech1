38a352475ba4273de7a7586e09d7b62c42f83a6a
miihhernandez <miihmars14@gmail.com>
2022-10-18 23:48:25 -0300

    alterado o javascript

--- login.js
+++ login.js
@@ -1,20 +1,28 @@
-function Login() {
-    let done=0;
-    let usuario = document.getElementsByName('username')[0].value;
-    usuario=usuario.toLowerCase();
-    let email = document.getElementsByName('email')[0].value;
-    email=email.toLowerCase();
-    let senha= document.getElementsByName('password')[0].value;
-    senha=senha.toLowerCase();
-    if (usuario=="miih" && senha=="1234" && email=="miihmars14@gmail.com") {
-      window.location="./index.html";
-      done=1;
-    }
-    if (done==1) { alert("Confirmação de login com sucesso"); }
+if (sessionStorage.username) {
+  document.getElementById("username").value = sessionStorage.username;
+}
 
-    if (usuario=="miih" != senha=="1234" != email=="miihmars14@gmail.com") {
-        window.location="./index.html"
-        done=0;
-    }
-    if(done==0){alert("Dados incorretos, tente novamente"); }
-  }
+if (sessionStorage.email) {
+  document.getElementById("email").value = sessionStorage.email;
+}
+
+if (sessionStorage.password) {
+  document.getElementById("password").value = sessionStorage.password;
+}
+
+
+let salvarData = function() {
+  let username =
+    document.getElementById("username").value;
+
+  let email = 
+    document.getElementById("email").value;
+  
+  let password = 
+    document.getElementById("password").value;
+    sessionStorage.setItem("username", username);
+    sessionStorage.setItem("email", email);
+    sessionStorage.setItem("password", password);
+}
+
+document.onchange = salvarData;
